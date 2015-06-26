030b8b3dda03275b0cf0a0fd5d70be4350853b69
Ziya SARIKAYA <sarikayaziya@gmail.com>
2015-06-26 09:59:06 +0300

    trivial

--- Whut.AttachTo/Guids.cs
+++ Whut.AttachTo/Guids.cs
@@ -6,7 +6,7 @@ namespace Whut.AttachTo
 {
     public static class GuidList
     {
-        public const string guidAttachToPkgString = "8d6080f0-7276-44d7-8dc4-6378fb6ce225";
+        public const string guidAttachToPkgString = "A7E222F8-5841-484D-9C8D-2A88619F0153";
 
         public const string guidAttachToCmdSetString = "16e2ac5c-ec3d-4ff1-a237-11ccef54fe0f";
 
